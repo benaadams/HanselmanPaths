64ea240cdf1a136737fa63dc568a5dae222bb711
Ben Adams <thundercat@illyriad.co.uk>
2018-09-15 18:24:34 +0100

    ignorecase for path test

--- test/HanselmanPaths.Tests/InboundPaths.cs
+++ test/HanselmanPaths.Tests/InboundPaths.cs
@@ -7,12 +7,12 @@ public class InboundPaths
 {
     [Theory]
     [InlineData(
-        "/blog/a-complete-containerized-net-core-appplication-microservice-that-is-as-small-as-possible.aspx", 
+        "/blog/a-complete-containerized-net-core-application-microservice-that-is-as-small-as-possible.aspx", 
         true, 
         "/blog/ACompleteContainerizedNETCoreApplicationMicroserviceThatIsAsSmallAsPossible.aspx")]
     public void KebabCase(string path, bool changed, string expectedPath)
     {
         Assert.Equal(changed, HanselmanPaths.TryKebabToPascalCase(path, out var newpath));
-        Assert.Equal(expectedPath, newpath);
+        Assert.Equal(expectedPath, newpath, ignoreCase: true);
     }
 }
